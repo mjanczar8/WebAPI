b6caeecbeab01f7dfbe7833e4d0bcf9af2d2b3ea
Michael Janczar <115019549+mjanczar8@users.noreply.github.com>
2025-01-16 21:20:03 -0500

    finished 1

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
 var msg = "Hi"
 
 console.log(msg)
+
+const express = require('express');
+const app = express();
+const path = require('path');
+const router = express.Router();
+ 
+router.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/public/index.html'));
+});
+ 
+router.get('/AddToList',function(req,res){
+    res.sendFile(path.join(__dirname+'/public/addtolist.html'));
+  });
+  router.get('/login',function(req,res){
+    res.sendFile(path.join(__dirname+'/public/login.html'));
+  });
+app.use('/', router);
+app.listen(process.env.port || 3000);
+ 
+console.log('Running at Port 3000');
